3b0f11323094f81a4e3f4500232a1617e0f3cfcb
Sayor Debbarma <91954258+sayordebbarma@users.noreply.github.com>
2024-03-08 11:02:13 +0530

    Create GFG_Check_if_frequencies_can_be_equal.java

---
+++ GFG-Check if frequencies can be equal/GFG_Check_if_frequencies_can_be_equal.java
@@ -0,0 +1,33 @@
+class Solution {
+    boolean sameFreq(String s) {
+        // code here
+        int[] freq = new int[26];
+        for(char ch : s.toCharArray()) freq[ch-'a']++;
+        
+        if(check(freq)) return true;
+        
+        for(int i=0; i<26; i++) {
+            if(freq[i]>0) {
+                freq[i]--;
+                
+                if(check(freq)) return true;
+                
+                freq[i]++;
+            }
+        }
+        return false;
+    }
+    
+    boolean check(int freq[]) {
+        int n = -1;
+        
+        for(int f : freq) {
+            if(f>0) {
+                if(n==-1) n = f;
+                else if(f!=n) return false;
+            }
+        }
+        
+        return true;
+    }
+}
