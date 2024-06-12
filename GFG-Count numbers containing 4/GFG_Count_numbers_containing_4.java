d4b238497e93b1eced70b237e16e1317a4f879dd
Sayor Debbarma <91954258+sayordebbarma@users.noreply.github.com>
2024-06-12 19:40:05 +0530

    Create GFG_Count_numbers_containing_4.java

---
+++ GFG-Count numbers containing 4/GFG_Count_numbers_containing_4.java
@@ -0,0 +1,22 @@
+class Solution {
+    public static int countNumberswith4(int n) {
+        // code here
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (containsDigit4(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    public static boolean containsDigit4(int number) {
+        while (number > 0) {
+            if (number % 10 == 4) {
+                return true;
+            }
+            number /= 10;
+        }
+        return false;
+    }
+}
