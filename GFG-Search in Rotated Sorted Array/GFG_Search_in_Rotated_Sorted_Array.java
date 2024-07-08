d9126164b6e8479cd47011efec245dcfcacdd826
Sayor Debbarma <91954258+sayordebbarma@users.noreply.github.com>
2024-07-08 23:07:57 +0530

    Create GFG_Search_in_Rotated_Sorted_Array.java

---
+++ GFG-Search in Rotated Sorted Array/GFG_Search_in_Rotated_Sorted_Array.java
@@ -0,0 +1,11 @@
+class Solution {
+    int search(int[] arr, int key) {
+        // Complete this function
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == key) return i;
+        }
+        
+        return -1;
+    }
+}
