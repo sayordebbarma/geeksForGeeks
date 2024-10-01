82975147d8246bd4535195086b2495ddc60392c6
Sayor Debbarma <91954258+sayordebbarma@users.noreply.github.com>
2024-10-01 22:18:32 +0530

    Create GFG_Multiply_two_linked_lists.java

---
+++ GFG-Multiply two linked lists/GFG_Multiply_two_linked_lists.java
@@ -0,0 +1,20 @@
+class Solution {
+    private static final int MOD = 1000000007;
+    
+    private long listToNumber(Node head) {
+        long num = 0;
+        while (head != null) {
+            num = (num * 10 + head.data) % MOD;
+            head = head.next;
+        }
+        return num;
+    }
+
+    public long multiplyTwoLists(Node first, Node second) {
+        // Code here
+        long num1 = listToNumber(first);
+        long num2 = listToNumber(second);
+        
+        return (num1 * num2) % MOD;
+    }
+}
