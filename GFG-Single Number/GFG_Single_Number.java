0382f76d88731915a270b0cf9fc4bf950f3d7b6e
Sayor Debbarma <91954258+sayordebbarma@users.noreply.github.com>
2024-10-18 21:27:41 +0530

    Create GFG_Single_Number.java

---
+++ GFG-Single Number/GFG_Single_Number.java
@@ -0,0 +1,12 @@
+class Solution {
+    int getSingle(int arr[]) {
+        // code here
+       int result = 0;
+        
+        for (int num : arr) {
+            result ^= num;
+        }
+        
+        return result;
+    }
+}
